55ccbe2f39ae72e2e2baeef4a41fda460da68d11
Bernard Jollans <jollans94@googlemail.com>
2017-11-20 22:54:10 +0100

    Initialized Everything

---
+++ src/main/scala/MiningDataStreams.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import scala.collection.mutable
+
+object MiningDataStreams {
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def HyperLogLog(par1: List[Set[Int]], par2: Double): List[List[Int]] = {
+    return null
+  }
+
+
+}
